f55dff55f13a9e94aef607f1f878d48ad6505bba
karim metawea <karim.metawea@gmail.com>
2019-12-14 18:05:27 +0200

    constructing app files

---
+++ MobileNoc-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileNoc-Task
+//
+//  Created by karim metawea on 12/14/19.
+//  Copyright © 2019 karim metawea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
